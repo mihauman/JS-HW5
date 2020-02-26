111c43f87eda07f9c8d995081cd4692612baa9ed
mihauman <mihauman1313@gmail.com>
2020-02-26 21:46:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,70 +1,74 @@
+//---------------
+const getEntireNumbers = (numbers) => {
+    return numbers.filter(element => element === Math.trunc(element));
+    }
+//---------------
+
 //#1
-const arr = [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2];
 function getRandomArray(length, min, max){
+  let arrRandom =[];
   for(let i = 0; i < length; i++){
-    arr.push(Math.floor(Math.random() * (max - min) ) + min);
+    arrRandom.push(Math.floor(Math.random() * (max - min) ) + min);
   }
-  return arr;                   
+  return arrRandom;                   
 };
 console.log(getRandomArray(15, 1, 100)); 
 //#2
-const count = (arr) => (arr.reduce(reducer, {}));
-const reducer = (obj, value) => {
-  obj[value] = obj[value] ? obj[value] + 1 : 1;
-  return obj;
-};
-const findMaxes = (counting) =>
-        (Object.keys(counting)
-         .reduce((maxes, key) => {
-                   if (maxes.max == counting[key]) return {max: maxes.max, values: maxes.values.concat(Number(key))};
-                   if (maxes.max >  counting[key]) return maxes;
-                   if (maxes.max <  counting[key]) return {max: counting[key], values: [key]};
-                   return maxes;
-                 },
-                 {max: -1, values: []}));
-
-const getValues = (length, maxes) => (maxes.values.length == length ? [] : maxes.values);
-const mode = (arr) => getValues(arr.length, findMaxes(count(arr)));
-console.log(mode(arr));
+function getModa(...numbers){
+    const moda = getEntireNumbers(numbers);
+        return moda.sort((a, b) =>
+        (moda.filter(v => v === a).length) - (moda.filter(v => v === b).length)).pop()
+      }
+console.log(getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2 ));
 //#3
-function getAverage(arr){
-let sum = 0; for(let i = 0; i < arr.length; i++){ sum += arr[i]; } console.log(sum/arr.length); } getAverage(arr);
+function getAverage(...numbers){
+let sum = 0;
+let average = 0;
+const filtered = getEntireNumbers(numbers);
+ for(let i = 0; i < filtered.length; i++){
+  sum += filtered[i];
+}
+  average = sum/filtered.length;
+return average;
+ } 
+ console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)); 
 //#4
-function getMedian(arr){
- let half = Math.floor(arr.length / 2);
-  arr.sort(function(a, b) { return a - b;});
+function getMedian(...numbers){
+const filtered = getEntireNumbers(numbers);
+ const half = Math.floor(filtered.length / 2);
+  filtered.sort(function(a, b) { return a - b;});
 
-  if (arr.length % 2) {
-    return arr[half];
+  if (filtered.length % 2) {
+    return filtered[half];
   } else {
-    return (arr[half] + arr[half] + 1) / 2.0;
+    return (filtered[half] + filtered[half] + 1) / 2.0;
   }
 }
-console.log(getMedian(arr));
+console.log(getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)); 
 //#5
-function filterEvenNumbers (arr){
-  const integers = arr.filter(number => {
+function filterEvenNumbers (...numbers){
+  const integers = numbers.filter(number => {
     return number % 2 == 0;
   });
   return integers;
 };
-console.log(filterEvenNumbers(arr));
+console.log(filterEvenNumbers(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 //#6
-function countPositiveNumbers(arr){
-  const negativeNumbers = arr.filter(number => {
+function countPositiveNumbers(...numbers){
+  const negativeNumbers = numbers.filter(number => {
 return number > 0; 
   });
   return negativeNumbers.length;
-}
-console.log(countPositiveNumbers(arr));
+} 
+console.log(countPositiveNumbers(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 //#7
-function getDividedByFive(arr){
-  const dividedByFive = arr.filter(number => {
+function getDividedByFive(...numbers){
+  const dividedByFive = numbers.filter(number => {
        return number % 5 === 0;
   })
    return dividedByFive;
   }
-  console.log(getDividedByFive(arr));
+  console.log(getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
   //#8
 let string = "Are you fucking kidding?";
 function replaceBadWords(string){
@@ -74,22 +78,14 @@ function replaceBadWords(string){
 }
   console.log(replaceBadWords(string));
 //#9
-function divideByThree(str1){
-  if(str1.length >= 3){
-    let array = [];
-    for(let i = 0; i < str1.length; i+=3){
-      array.push(str1.substring(i, i+3));
-    }
-    return array;
-  }else{
-    throw new Error('This string is to short');
-  }
- 
+const divideByThree = (word) => {
+    const temp = word.split(' ').join('').toLowerCase();
+    return temp.match(/.{1,3}/g);
 };
 console.log(divideByThree("Commander"));
 //#10
 function generateCombinations(str) {
-      let results = [];
+      const results = [];
 
       if (str.length === 1)
       {
